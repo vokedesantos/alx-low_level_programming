bc87201c49f2158df509d98aaa565470a2ecefb2
Vokedesantos <kelvinkariuki143@gmail.com>
2023-02-16 07:07:56 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,16 +10,4 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-#include <stdio.h>
-
-/**
- *main - Entry point
- *Return: Always 0 (Success)
- */
-
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-}
  
