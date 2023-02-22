44fab381b676b3913d7ed685fbc00fa037362637
Vokedesantos <kelvinkariuki143@gmail.com>
2023-02-22 08:46:14 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times, in lowercase.
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char alpha;
+
+	while (count++ <= 9)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+		_putchar('\n');
+	}
+}
