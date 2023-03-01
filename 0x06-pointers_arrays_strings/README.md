3aa6239b023dfbe06ac0b36c2d47891799701dd9
Vokedesantos <kelvinkariuki143@gmail.com>
2023-03-01 11:30:38 -0800

    main.

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Write a function that concatenates two strings
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -20,11 +20,3 @@ int main(void)
     printf("%s", ptr);
     return (0);
 }
-julien@ubuntu:~/0x06$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-strcat.c -o 0-strcat
-julien@ubuntu:~/0x06$ ./0-strcat 
-Hello 
-World!
-Hello World!
-World!
-Hello World!
-
