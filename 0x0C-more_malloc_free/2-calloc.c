e6bdfab1d51a727e2e21c01ba541c508c8ed4dcb
Vokedesantos <kelvinkariuki143@gmail.com>
2023-03-16 06:47:56 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,21 +1,93 @@
+#include <stdlib.h>
+		
 #include "main.h"
+		
+
+		
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measured.
+		
+ * *_memset - fills memory with a constant byte
+		
+ * @s: memory area to be filled
+		
+ * @b: char to copy
+		
+ * @n: number of times to copy b
+		
  *
- * Return: The length of the string.
+		
+ * Return: pointer to the memory area s
+		
  */
-int _strlen_recursion(char *s)
+		
+char *_memset(char *s, char b, unsigned int n)
+		
 {
-	int longit = 0;
+		
+	unsigned int i;
+		
 
-	if (*s)
+		
+	for (i = 0; i < n; i++)
+		
 	{
-		longit++;
-		longit += _strlen_recursion(s + 1);
+		
+		s[i] = b;
+		
 	}
+		
 
-	return (longit);
+		
+	return (s);
+		
+}
+		
+
+		
+/**
+		
+ * *_calloc - allocates memory for an array
+		
+ * @nmemb: number of elements in the array
+		
+ * @size: size of each element
+		
+ *
+		
+ * Return: pointer to allocated memory
+		
+ */
+		
+void *_calloc(unsigned int nmemb, unsigned int size)
+		
+{
+		
+	char *ptr;
+		
+
+		
+	if (nmemb == 0 || size == 0)
+		
+		return (NULL);
+		
+
+		
+	ptr = malloc(size * nmemb);
+		
+
+		
+	if (ptr == NULL)
+		
+		return (NULL);
+		
+
+		
+	_memset(ptr, 0, nmemb * size);
+		
+
+		
+	return (ptr);
+		
 }
 
 
