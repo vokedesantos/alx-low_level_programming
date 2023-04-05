a920d15d53a63f315cd57a0733569d2c0ce5fd4c
vokedesantos <kelvinkariuki143@gmail.com>
2023-04-05 10:58:07 -0700

    help

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,17 +1,12 @@
 #include "lists.h"
+
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
  *
- * Description: singly linked list node structure
- * 
+ * Return: number of nodes
  */
-typedef struct listint_s
-{
-    int n;
-    struct listint_s *next;
-} listint_t;
+size_t print_listint(const listint_t *h)
 {
  size_t num = 0;
 
